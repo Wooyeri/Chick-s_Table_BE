1c0de88ce8b5304eebedc2f477cea41cb147ea23
woo <clwkwhdk@gmail.com>
2024-09-25 02:40:19 +0900

    first commit

---
+++ chickstable/src/main/java/com/bugwarriors/chickstable/ChickstableApplication.java
@@ -0,0 +1,13 @@
+package com.bugwarriors.chickstable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickstableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickstableApplication.class, args);
+	}
+
+}
---
+++ chickstable/src/test/java/com/bugwarriors/chickstable/ChickstableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugwarriors.chickstable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickstableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
